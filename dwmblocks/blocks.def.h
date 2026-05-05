4571047afce13623f841ba749a604f168e1c3670
Jax|Technologies <104657792+jaxtechnologies@users.noreply.github.com>
2026-05-05 09:44:13 -0400

    Update icons in blocks.def.h for statusbar

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,15 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", 	"~/scripts/jt-kernel",		3600,		0},
+	{"🐧", 	"~/scripts/jt-kernel",		3600,		0},
 
 	{"", 	"~/scripts/jt-battery",		60,		0},
 
-	{"💿 ", 	"~/scripts/jt-disk",		60,		0},
+	{"💿", 	"~/scripts/jt-disk",		60,		0},
 
 	{"",	"~/scripts/mod_memory.sh",		30,		0},
 
-	{"🕛 ", 	"~/scripts/jt-clock",		5,		0},
+	{"🕛", 	"~/scripts/jt-clock",		5,		0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.

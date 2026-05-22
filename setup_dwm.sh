26aff779368d1d13af783ee843b32efdbfa5769a
Jax|Technologies <104657792+jaxtechnologies@users.noreply.github.com>
2026-05-22 16:53:16 -0400

    Remove wallpaper and xsessions setup commands
    Removed wallpaper and xsessions setup from script.

--- setup_dwm.sh
+++ setup_dwm.sh
@@ -11,16 +11,6 @@ mousepad
 figlet
 sxhkd
 
-##### wallpapers -- Make sure wallpapers directory exists or add wallpapers if it doesn't exist
-[ ! -d ~/wallpapers ] && mkdir -p ~/wallpapers 2>&1 | tee -a $LOG
-cp $INSTALL_PATH/wallpapers/* ~/wallpapers/ 2>&1 | tee -a $LOG
-
-##### xsessions -- Make sure xsessions directory exists and add dwm.desktop file
-##### This file allows users to select DWM as a session option when logging into their system through a display manager.
-[ ! -d /usr/share/xsessions ] && sudo mkdir -p /usr/share/xsessions 2>&1 | tee -a $LOG
-sudo cp $INSTALL_PATH/dotfiles/dwm.desktop /usr/share/xsessions/ 2>&1 | tee -a $LOG
-
-
 ##### openssh -- Enable and start openssh
 echo "Enabling and starting openssh..."
 sleep 5

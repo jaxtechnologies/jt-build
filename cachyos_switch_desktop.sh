2b14c46d1e164c0806e73709366009ef5620adf6
Jax|Technologies <104657792+jaxtechnologies@users.noreply.github.com>
2026-06-04 09:42:34 -0400

    Update desktop installation to use sddm
    Replaced lightdm with sddm for Niri installation.

--- cachyos_switch_desktop.sh
+++ cachyos_switch_desktop.sh
@@ -215,12 +215,9 @@ install_niri () {
 	remove_current_desktop
 	sleep 10
 	sudo pacman -Syu --noconfirm
-	#sudo pacman -S niri xwayland-satellite cachyos-niri-noctalia lightdm lightdm-gtk-greeter --needed --noconfirm
 	sudo pacman -S niri xwayland-satellite cachyos-niri-noctalia sddm --needed --noconfirm
     sleep 10
 	rsync -a /etc/skel/.config ~/
-	sleep 20
-	#sudo systemctl enable lightdm
 	sudo systemctl enable sddm
     clear
     echo ""

abb198053761795585020d5cb220fcfc9c88cc77
Jax|Technologies <104657792+jaxtechnologies@users.noreply.github.com>
2026-06-08 15:34:51 -0400

    Update message for bspwm installation notification

--- cachyos_switch_desktop.sh
+++ cachyos_switch_desktop.sh
@@ -151,7 +151,7 @@ install_bspwm () {
 	feh --bg-fill ~/wallpaper.jpg
     clear
     echo ""
-    echo " Cinnamon installed. Please reboot and select Cinnamon Session from the login screen to start using it."
+    echo " bspwm installed. Please reboot and select bspwm Session from the login screen to start using it."
     sleep 10
     exit_menu
 }

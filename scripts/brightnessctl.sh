c6aff8f5ec18dc9bbaeb3831b093a12f035740de
Jax|Technologies <104657792+jaxtechnologies@users.noreply.github.com>
2026-06-18 16:11:08 -0400

    Refactor brightness icons based on brightness levels

--- scripts/brightnessctl.sh
+++ scripts/brightnessctl.sh
@@ -2,26 +2,38 @@
 BRIGHTNESS_VALUE=`brightnessctl | grep -o "(.*" | tr -d "()"`
 BRIGHTNESS_NR=${BRIGHTNESS_VALUE//%}
 
-if [ $BRIGHTNESS_NR -lt 10 ]; then
-	BRIGHTNESS_ICON=''
-elif [ $BRIGHTNESS_NR -lt 20 ]; then
-	BRIGHTNESS_ICON=''
-elif [ $BRIGHTNESS_NR -lt 30 ]; then
-	BRIGHTNESS_ICON=''
+#if [ $BRIGHTNESS_NR -lt 10 ]; then
+#	BRIGHTNESS_ICON=''
+#elif [ $BRIGHTNESS_NR -lt 20 ]; then
+#	BRIGHTNESS_ICON=''
+#elif [ $BRIGHTNESS_NR -lt 30 ]; then
+#	BRIGHTNESS_ICON=''
+#elif [ $BRIGHTNESS_NR -lt 40 ]; then
+#	BRIGHTNESS_ICON=''
+#elif [ $BRIGHTNESS_NR -lt 50 ]; then
+#	BRIGHTNESS_ICON=''
+#elif [ $BRIGHTNESS_NR -lt 60 ]; then
+#	BRIGHTNESS_ICON=''
+#elif [ $BRIGHTNESS_NR -lt 70 ]; then
+#	BRIGHTNESS_ICON=''
+#elif [ $BRIGHTNESS_NR -lt 80 ]; then
+#	BRIGHTNESS_ICON=''
+#elif [ $BRIGHTNESS_NR -lt 90 ]; then
+#	BRIGHTNESS_ICON=''
+#else
+#	BRIGHTNESS_ICON=''
+#fi
+
+if [ $BRIGHTNESS_NR -lt 20 ]; then
+	BRIGHTNESS_ICON='🌑'
 elif [ $BRIGHTNESS_NR -lt 40 ]; then
-	BRIGHTNESS_ICON=''
-elif [ $BRIGHTNESS_NR -lt 50 ]; then
-	BRIGHTNESS_ICON=''
+	BRIGHTNESS_ICON='🌒'
 elif [ $BRIGHTNESS_NR -lt 60 ]; then
-	BRIGHTNESS_ICON=''
-elif [ $BRIGHTNESS_NR -lt 70 ]; then
-	BRIGHTNESS_ICON=''
+	BRIGHTNESS_ICON='🌓'
 elif [ $BRIGHTNESS_NR -lt 80 ]; then
-	BRIGHTNESS_ICON=''
-elif [ $BRIGHTNESS_NR -lt 90 ]; then
-	BRIGHTNESS_ICON=''
+	BRIGHTNESS_ICON='🌔'
 else
-	BRIGHTNESS_ICON=''
+	BRIGHTNESS_ICON='🌕'
 fi
 
 echo "$BRIGHTNESS_ICON $BRIGHTNESS_VALUE"

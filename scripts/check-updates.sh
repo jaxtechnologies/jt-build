2af31fe342918bdacfe9feb4f2d5d581ccf26fef
Jax|Technologies <104657792+jaxtechnologies@users.noreply.github.com>
2026-06-19 15:24:05 +0000

    Add files via upload

---
+++ scripts/check-updates.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Check official repositories
+if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
+    updates_arch=0
+fi
+
+# Check AUR repositories (uncomment if using yay or paru)
+# if ! updates_aur=$(yay -Qum 2> /dev/null | wc -l); then
+# if ! updates_aur=$(paru -Qum 2> /dev/null | wc -l); then
+#     updates_aur=0
+# fi
+
+# Total updates
+updates=$((updates_arch + updates_aur))
+
+# Output the result for Polybar
+if [ "$updates" -gt 0 ]; then
+    echo "   $updates"
+else
+    echo "   0"
+fi
+
